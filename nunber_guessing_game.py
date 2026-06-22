2226edc6041ec93213dc1296e321e7e27bbc5108
Jagrit Mandora <jagritmandora@gmail.com>
2026-06-23 02:16:58 +0530

    number_guessing_game

---
+++ nunber_guessing_game.py
@@ -0,0 +1,35 @@
+import random
+
+print("welcome to the number guessing game")
+print("please enter your guess")
+
+lowest_number=1
+highest_number=100
+
+answer=random.randint(lowest_number,highest_number)
+
+guesses=0
+
+is_running=True
+
+while is_running and guesses<10:
+
+    guess=input("Please enter the guessed number: ")
+    
+    if guess.isdigit():
+        guess= int(guess)
+        if guess < lowest_number:
+            print(f"INPUT IS TOO LOW. IT SHOULD BE HIGHER THAN {lowest_number}")
+        elif guess > highest_number:
+            print(f"INPUT IS TOO HIGH. IT SHOULD BE LOWER THAN {highest_number}")
+        elif guess == answer:
+            print("CORRECT ANSWER")
+            is_running=False
+        else:
+            print("TRY AGAIN")
+        guesses+=1
+    else:
+        print("Please enter a valid number")
+    
+print(f"No of guesses: {guesses}")
+
---
+++ tryfile.py
@@ -0,0 +1,15 @@
+#guess=input("enter")
+#a=int(guess)
+#print(type(a))
+
+'''a = int("hello")
+
+print(a)
+
+print(type(a))'''
+
+'''print(1 == False)'''
+
+guess = "45"
+
+print(guess < 67)
